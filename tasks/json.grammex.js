e825cc21ee84f36be9b6aece48eae6da4a757b98
Fabio Spampinato <spampinabio@gmail.com>
2023-07-29 01:11:10 +0100

    Benchmark: ensuring both grammars are exactly the same

--- tasks/json.grammex.js
+++ tasks/json.grammex.js
@@ -15,10 +15,10 @@ const Null = 'null'
 const False = 'false';
 const True = 'true';
 const Number = /-?(?:0|[1-9][0-9]*)(?:\.[0-9]+)?(?:[eE][-+]?[0-9]+)?/;
-const String = /\s*"(?:\\.|\\u[0-9A-Fa-f]{4}|[^"])*"\s*/;
+const String = /"(?:\\.|\\u[0-9A-Fa-f]{4}|[^"])*"/;
 
 const Array = ['[', _, optional ([ LAZY_JSON, star ([ ',', LAZY_JSON ]) ]), _, ']'];
-const Object = ['{', _, optional ([ String, ':', LAZY_JSON, star ([ ',', String, ':', LAZY_JSON ]) ]), _, '}'];
+const Object = ['{', _, optional ([ String, _, ':', LAZY_JSON, star ([ ',', _, String, _, ':', LAZY_JSON ]) ]), _, '}'];
 
 const JSON = [_, { Object, Array, String, True, False, Null, Number }, _];
 
