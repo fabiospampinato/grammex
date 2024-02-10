052083cc0178fd946f4d97cf2ab09e425eafa605
Fabio Spampinato <spampinabio@gmail.com>
2024-02-10 23:38:48 +0000

    Slightly optimized sample JSON grammar

--- tasks/json.grammex.js
+++ tasks/json.grammex.js
@@ -14,7 +14,7 @@ const Null = 'null';
 const False = 'false';
 const True = 'true';
 const Number = /-?(?:0|[1-9][0-9]*)(?:\.[0-9]+)?(?:[eE][-+]?[0-9]+)?/;
-const String = /"(?:\\.|\\u[0-9A-Fa-f]{4}|[^"])*"/;
+const String = /"(?:\\u[0-9A-Fa-f]{4}|\\.|[^"\\]+|[^"])*"/;
 
 const Array = ['[', _, optional ([ LAZY_JSON, star ([ ',', LAZY_JSON ]) ]), _, ']'];
 const Object = ['{', _, optional ([ String, _, ':', LAZY_JSON, star ([ ',', _, String, _, ':', LAZY_JSON ]) ]), _, '}'];
